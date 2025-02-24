f4918dfa3ffb09a29204d28c1c28506779764b38
Franco <alanfrancogodoy@gmail.com>
2025-02-24 20:39:13 -0300

    fixed main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import fastifyCors from '@fastify/cors';
+import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify';
+import cors from '@fastify/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.register(fastifyCors, {
-    origin: "https://chatbot-frontend-production-a514.up.railway.app/",
+
+  await app.register(cors, {
+    origin: ["https://chatbot-frontend-production-a514.up.railway.app"], 
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   });
+
   await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
