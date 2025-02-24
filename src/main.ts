fefd5a390ea877f0b9a4c8e985ffefa435594852
Franco <alanfrancogodoy@gmail.com>
2025-02-24 20:24:08 -0300

    changess

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.listen(3000, '0.0.0.0'); // No uses process.env.PORT en Vercel
+  await app.register(fastifyCors, {
+    origin: "https://chatbot-frontend-production-a514.up.railway.app/",
+  });
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
