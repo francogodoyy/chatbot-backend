f5884390a2b975714f05d51567781684f79e5daa
francogodoyy <alanfrancogodoy@gmail.com>
2025-02-24 11:39:02 -0300

    vercel test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as cors from 'cors';
-
-dotenv.config();
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT, () => console.log(`🚀 Backend corriendo en el puerto ${PORT}`));
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(3000, '0.0.0.0'); // No uses process.env.PORT en Vercel
 }
 bootstrap();
